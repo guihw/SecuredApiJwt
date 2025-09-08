7f34093eedad75666fd806dd04f21b41e6ee4df1
Guilherme Santos <guilhermess2005g@gmail.com>
2025-09-08 16:23:23 -0300

    alterado @controller para @restcontroller na /api endpoint

--- src/main/java/com/guilh/SecuredApiJWT/Controller/CustomerController.java
+++ src/main/java/com/guilh/SecuredApiJWT/Controller/CustomerController.java
@@ -3,8 +3,9 @@ package com.guilh.SecuredApiJWT.Controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @RequestMapping("/api")
 public class CustomerController {
     @GetMapping("/hello")
